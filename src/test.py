a392320438f5e843c0ab1902288d903513b3df47
mzDAN <mh.ziaei.dan@gmail.com>
2020-09-04 21:59:30 +0430

    after teseting <checkout> for reset to last commit and <reset> for unstageing

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 print("hello world")
 print("i'm testing git here to learn distributed version control systems!!!")
-
+    
 print("now i learn git log :)\n i want to add some new files!!!")
 
 print("know i pracitce git 4th lesson")
