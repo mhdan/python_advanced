6cbca9973039ceb557ebf24f5a0c16a490ec6040
mzDAN <mh.ziaei.dan@gmail.com>
2020-09-05 11:57:46 +0430

    writing the practince of brach on main file

--- src/test.py
+++ src/test.py
@@ -1,8 +1,10 @@
 print("hello world")
 print("i'm testing git here to learn distributed version control systems!!!")
-    
+
 print("now i learn git log :)\n i want to add some new files!!!")
 
 print("know i pracitce git 4th lesson")
 print("i changed csv file alse but i use git diff and check out for it\nfinally i commit this file only!!!")
 print("now i test <git reset> command for undo the changes for some file in stage!!!")
+
+print("now i add three text files to practice brach and merge after it")
